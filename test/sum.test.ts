a709f62c7aa83d7011a4955628f2617b3297bc33
negeek7 <negeek7@gmail.com>
2024-11-07 17:26:41 +0530

    one small test tested with jest using babel for ts

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,1 +1,6 @@
-console.log("hello")
+import {describe, expect, test} from '@jest/globals';
+import {sum} from "../src/sum";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
