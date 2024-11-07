e3ef80969e6ef782058d4cccf1dcc5283efceb0e
negeek7 <negeek7@gmail.com>
2024-11-07 18:20:56 +0530

    added test for single digit string

--- test/add.test.ts
+++ test/add.test.ts
@@ -10,9 +10,8 @@ test('should add all the numbers in string', () => {
     expect(add("1,2,3,4")).toBe(10);
 });
 
+
 test("should return the number for single digit string", () => {
     expect(add("1")).toBe(1);
 });
-
-
 
